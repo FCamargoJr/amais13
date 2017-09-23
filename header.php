a5029a0cae75ac20498d337037d4e597c909651d
rickyrossi <henrique@hridiomas.com.br>
2017-09-23 14:10:29 -0300

    ...
    ...

--- header.php
+++ header.php
@@ -1,3 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>AMAIS13 - Agência</title>
+
+  	<link href="css/bootstrap.css" rel="stylesheet">
+  	<link href="css/estilo2.css" rel="stylesheet" type="text/css">
+  </head>
+</head>
+
+<body>
 <header>
     <?php include 'menu.php';?>
 </header>
