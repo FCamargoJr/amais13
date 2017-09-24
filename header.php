02e1e0a38e7689aee0edf28f5f4ae83ee5ed5dfb
rickyrossi <henrique@hridiomas.com.br>
2017-09-24 14:35:09 -0300

    Theme
    Theme

--- header.php
+++ header.php
@@ -8,7 +8,20 @@
 
   	<link href="css/bootstrap.css" rel="stylesheet">
   	<link href="css/estilo2.css" rel="stylesheet" type="text/css">
-  </head>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+
+<script>
+var $doc = $('html, body');
+$('.scrollSuave').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+</script>
+
+
 </head>
 
 <body>
