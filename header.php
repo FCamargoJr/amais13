9ebb7e8c03c4e6a60e5190fc0957d7facc97c257
rickyrossi <henrique@hridiomas.com.br>
2017-09-25 09:33:37 -0300

    script
    script

--- header.php
+++ header.php
@@ -9,19 +9,6 @@
   	<link href="css/bootstrap.css" rel="stylesheet">
   	<link href="css/estilo2.css" rel="stylesheet" type="text/css">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
-
-<script>
-var $doc = $('html, body');
-$('.scrollSuave').click(function() {
-    $doc.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-});
-</script>
-
-
 </head>
 
 <body>
